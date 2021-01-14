be863a10bf9f559646b2163dcc6192d818cf18db
masatomo-sugawara <masatomo.s1982@gmail.com>
2021-01-14 10:31:11 +0900

    画面遷移テスト

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import About from './About';
+import Home from './Home';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World !!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar /><hr/>
+            <Route exact path='/' component={Home}/>
+            <Route path='/About' component={About}/>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
