7f8e4e53b958f6e681c51cdcb8e8b1e43d300f2e
Yuuna-Kida <Yuna.Kida@jp.ricoh.com>
2021-01-14 12:04:51 +0900

    リンク先の追加

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,17 @@ class Navbar extends React.Component {
     render(){
         return(
             <div>
+                <div>
                 <Link to="/">Home</Link>
                 <Link to="/About">About</Link>
+                </div>
+                <div>
+                <Link to="/Equipment">機材</Link>
+                <Link to="/Reservation">予約</Link>
+                <Link to="/ResultManagement">実績管理</Link>
+                <Link to="/UserManagement">ユーザー管理</Link>
+                <Link to="/Recycle">リサイクル</Link>
+                </div>
             </div>
         )
     }
